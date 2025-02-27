96b6299d82e3d2ac7d8739aaa22e0b14f00115da
MONCEF52341 <mostaine.moncef177@gmail.com>
2025-02-27 12:04:52 +0000

    toolbar + silk dans urls

--- django-starter.sh
+++ django-starter.sh
@@ -84,3 +84,26 @@ if DEBUG:
     SILKY_AUTHENTICATION = False
     SILKY_AUTHORISATION = False
 EOL
+
+# Modification de urls.py
+URLS_FILE="$PROJECT_NAME/urls.py"
+
+echo "🔗 Mise à jour de $URLS_FILE..."
+cat <<EOL > "$URLS_FILE"
+from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+    from django.conf.urls.static import static
+
+    urlpatterns += [path("__debug__/", include(debug_toolbar.urls))]
+    urlpatterns += [path("silk/", include("silk.urls", namespace="silk"))]
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+EOL
