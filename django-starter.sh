abe216395f3c7f6a2afefc8d24df41e81ad82d28
MONCEF52341 <mostaine.moncef177@gmail.com>
2025-02-27 11:58:15 +0000

    installation des dépences

--- django-starter.sh
+++ django-starter.sh
@@ -4,3 +4,12 @@ then
     echo "pipenv n'est pas installé. Installez-le avec 'pip install pipenv'."
     exit 1
 fi
+
+# Nom du projet Django
+PROJECT_NAME="main"
+
+echo "🛠️  Initialisation du projet Django..."
+
+# Initialisation de pipenv et installation des dépendances
+pipenv install django django-extensions django-jazzmin python-decouple
+pipenv install flake8 black isort pytest pytest-django django-silk django-debug-toolbar --dev
